a74aa4a9540fe3ace0049d288be5d37ecd80cac6
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-05-11 15:12:12 +0200

    Finish 6.1

--- src/reducer.js
+++ src/reducer.js
@@ -5,16 +5,18 @@ const initialState = {
 }
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
   switch (action.type) {
     case 'GOOD':
-      return state
+      const good = state.good + 1
+      return { ...state, good }
     case 'OK':
-      return state
+      const ok = state.ok + 1
+      return { ...state, ok }
     case 'BAD':
-      return state
+      const bad = state.bad + 1
+      return { ...state, bad }
     case 'ZERO':
-      return state
+      return initialState
     default: return state
   }
   
